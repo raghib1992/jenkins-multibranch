d44f7ead5c3ecf873a5dac786ccc51a2807eaf91
raghib1992 <raghib.npti@gmail.com>
2021-09-14 19:51:32 +0530

    major change

--- infrastructure/ap-south-1/providers.tf
+++ infrastructure/ap-south-1/providers.tf
@@ -6,9 +6,9 @@ data "aws_caller_identity" "current" {}
 
 terraform {
   backend "s3" {
-    bucket                      = "sc-saas-terraform"
-    key                         = "global/us-west-2"
-    region                      = "us-west-2"
+    bucket                      = "raghib-sc-terraform-backend"
+    key                         = "global/ap-south-1"
+    region                      = "ap-south-1"
     skip_credentials_validation = true
   }
   required_providers {
